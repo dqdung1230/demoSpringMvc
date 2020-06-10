d67442fc6ee7172f53c91f9361b16534c31cf837
dqdung1230 <dqdung96@gmail.com>
2020-06-10 14:24:59 +0700

    commit:

--- src/main/java/vn/itsol/demo/controller/HelloController.java
+++ src/main/java/vn/itsol/demo/controller/HelloController.java
@@ -3,10 +3,13 @@ package vn.itsol.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import vn.itsol.demo.entity.Member;
 import vn.itsol.demo.service.MemberService;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping({"/", "/home"})
@@ -15,15 +18,19 @@ public class HelloController {
     public MemberService memberService;
 
     @GetMapping("/hello")
-    public ModelAndView hello(){
+    public ModelAndView hello() {
         ModelAndView view = new ModelAndView();
         view.setViewName("Hello");
         return view;
     }
 
     @GetMapping()
-    public ModelAndView register(){
-        return new ModelAndView("Register","command", new Member());
+    public ModelAndView register() {
+        return new ModelAndView("Register", "command", new Member());
     }
 
+    @ModelAttribute("countryList")
+    public Map<String, String> getCountryList() {
+        return memberService.getCountryList();
+    }
 }
